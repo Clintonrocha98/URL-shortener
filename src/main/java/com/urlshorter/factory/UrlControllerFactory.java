b2d729c27115e82be11015e9cd85ba3e8d4a69ef
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-01-20 08:09:53 -0300

    feat: add db connection to controller factory

--- src/main/java/com/urlshorter/factory/UrlControllerFactory.java
+++ src/main/java/com/urlshorter/factory/UrlControllerFactory.java
@@ -1,13 +1,16 @@
 package com.urlshorter.factory;
 
+import java.sql.SQLException;
+
+import com.urlshorter.config.DatabaseConfig;
 import com.urlshorter.controller.UrlController;
 import com.urlshorter.repository.UrlRepository;
 import com.urlshorter.service.UrlService;
 
 public class UrlControllerFactory {
 
-    public static UrlController createController() {
-        UrlRepository repo = new UrlRepository();
+    public UrlController createController() throws SQLException {
+        UrlRepository repo = new UrlRepository(DatabaseConfig.getConnection(), true);
         UrlService service = new UrlService(repo);
         return new UrlController(service);
     }
