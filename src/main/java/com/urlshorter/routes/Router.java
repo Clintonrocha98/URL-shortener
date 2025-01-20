25d93d4613a92447dacc32a344c96f81ca6855d6
Clintonrocha98 <clintongoncalvesrocha@gmail.com>
2025-01-20 08:06:31 -0300

    chore: path routes

--- src/main/java/com/urlshorter/routes/Router.java
+++ src/main/java/com/urlshorter/routes/Router.java
@@ -7,20 +7,30 @@ import com.urlshorter.factory.UrlControllerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.SQLException;
 
 public class Router implements HttpHandler {
 
-    private UrlController controller = UrlControllerFactory.createController();
+    private UrlController controller;
+
+    public Router() {
+        try {
+            this.controller = new UrlControllerFactory().createController();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao criar o UrlController", e);
+        }
+    }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath();
         String method = exchange.getRequestMethod();
 
-        if (path.equals("/shorten-url") && "POST".equalsIgnoreCase(method)) {
+        if (path.contains("/shorten-url") && "POST".equalsIgnoreCase(method)) {
             controller.saveUrl(exchange);
-        } else if (path.contains("/get-url/") && "GET".equalsIgnoreCase(method)) {
-            controller.getUrl(exchange);
+        } else if (path.matches("^/[a-zA-Z0-9]{1,10}$") && "GET".equalsIgnoreCase(method)) {
+            String keyUrl = path.substring(1);
+            controller.getUrl(exchange,keyUrl);
         } else {
             String response = "Rota não encontrada!";
 
